a9855c6bd1c6338cb165a724533fa9e3914e1085
vikas967 <mathurvikas000@gmail.com>
2025-08-08 19:38:02 +0530

    connect to mongodb

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env' // Ensure the path to your .env file is correct
+})
+
+
+connectDB()
+
+// import express from 'express';
+// const app = express();
+
+// ( async ()=>{
+//     try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
+//     app.on('error', (err) => {
+//         console.error('Error in MongoDB connection:', err);
+//         throw err;
+//     })
+
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`Server is running on port ${process.env.PORT}`);
+//     })
+// }
+
+// catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//         process.exit(1);
+//       }
+// })()
