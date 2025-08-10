5bc698ef396c48d80a4964a59c77ffda3472cf7e
vikas967 <mathurvikas000@gmail.com>
2025-08-10 11:07:04 +0530

    cloudnary handle

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    const uploadOnCloudinary = async (filePath) => {
+        try {
+            const result = await cloudinary.uploader.upload(filePath, { 
+                resource_type: 'auto'
+            })
+
+            // file has been uploaded succesfully
+            console.log('File uploaded successfully:', result.secure_url);
+            return result;
+            
+        } catch (error) {
+            fs.unlinkSync(filePath); // delete the file if upload fails or locally saved temporary files
+            return {
+                error: 'Failed to upload file to Cloudinary',
+                details: error.message || 'Unknown error'
+            }
+        }
+    };
+
+    export { uploadOnCloudinary };
+
+    
+   
